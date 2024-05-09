f55d58522a5e4168ac6f8ea1eacce64eedfb9bd4
Kylin <unikylin@126.com>
2024-05-09 17:44:20 +0800

    using stack size cal max height of binary tree

--- src/main/java/org/leetcode/LC104.java
+++ src/main/java/org/leetcode/LC104.java
@@ -3,6 +3,8 @@ package org.leetcode;
 import org.junit.Test;
 import org.kylin.binarytree.TreeNode;
 
+import java.util.Stack;
+
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -13,6 +15,11 @@ import static org.junit.Assert.assertEquals;
  */
 public class LC104 {
 
+    /**
+     * Recursive of binary tree
+     * @param root
+     * @return max depth of binary tree
+     */
     public int maxDepth(TreeNode root) {
         if (root == null) {
             return 0;
@@ -27,11 +34,42 @@ public class LC104 {
         return Integer.max(leftHeight, rightHeight) + 1;
     }
 
+    /**
+     * Using postorder traversal
+     * @param root
+     * @return max depth of binary tree
+     */
+    public int maxDepth2(TreeNode root) {
+        TreeNode curr = root;
+        TreeNode pop = null;
+        Stack<TreeNode> stack = new Stack<>();
+        int max = 0;
+
+        while (curr != null || !stack.isEmpty()) {
+            if (curr != null) {
+                stack.push(curr);
+                int size = stack.size();
+                if (max < size) {
+                    max = size;
+                }
+                curr = curr.left;
+            } else {
+                TreeNode peek = stack.peek();
+                if (peek.right == null || peek.right == pop) {
+                    pop = stack.pop();
+                } else {
+                    curr = peek.right;
+                }
+            }
+        }
+        return max;
+    }
+
     @Test
     public void Test1() {
-        TreeNode root = new TreeNode(null, 1, null);
+        TreeNode root = new TreeNode(null, 1, new TreeNode(null, 2, null));
         int result = new LC104().maxDepth(root);
-        assertEquals(1, result);
+        assertEquals(2, result);
     }
 
     @Test
@@ -45,6 +83,27 @@ public class LC104 {
     public void Test3() {
         TreeNode root = new TreeNode(new TreeNode(new TreeNode(null, 3, null), 2, null), 1, null);
         int result = new LC104().maxDepth(root);
+        assertEquals(3, result);
+    }
+
+    @Test
+    public void Test21() {
+        TreeNode root = new TreeNode(null, 1, new TreeNode(null, 2, null));
+        int result = new LC104().maxDepth2(root);
+        assertEquals(2, result);
+    }
+
+    @Test
+    public void Test22() {
+        TreeNode root = new TreeNode(new TreeNode(null, 2, null), 1, null);
+        int result = new LC104().maxDepth2(root);
+        assertEquals(2, result);
+    }
+
+    @Test
+    public void Test23() {
+        TreeNode root = new TreeNode(new TreeNode(new TreeNode(null, 3, null), 2, null), 1, null);
+        int result = new LC104().maxDepth2(root);
         assertEquals(3, result);
     }
 }
