061aeace59b1aa2ee5752bef2ab3dffe816c5dd9
Kylin <unikylin@126.com>
2024-05-13 13:16:48 +0800

    unique paths

---
+++ src/main/java/org/leetcode/LC62.java
@@ -0,0 +1,51 @@
+package org.leetcode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * <H1>62. 不同路径</H1>
+ * https://leetcode.cn/problems/unique-paths/description/
+ */
+public class LC62 {
+
+    public int uniquePaths(int m, int n) {
+        // 1. dp array. m * n
+        int[][] dp = new int[m][n];
+
+        // 2. init value
+        // column
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+
+        // row
+        for (int j = 0; j < n; j++) {
+            dp[0][j] = 1;
+        }
+
+        // 3. calculate
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+
+        return dp[m - 1][n - 1];
+    }
+
+    @Test
+    public void TestUniquePaths() {
+        int ant = new LC62().uniquePaths(3, 7);
+        assertEquals(28, ant);
+
+        int bear = new LC62().uniquePaths(3, 2);
+        assertEquals(3, bear);
+
+        int cat = new LC62().uniquePaths(3, 3);
+        assertEquals(6, cat);
+    }
+
+
+}
