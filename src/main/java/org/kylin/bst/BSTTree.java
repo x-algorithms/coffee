767f44fa3dac43fa4ba04fe67808de786f7a13c8
Kylin <unikylin@126.com>
2024-05-28 13:36:08 +0800

    define bst node

---
+++ src/main/java/org/kylin/bst/BSTTree.java
@@ -0,0 +1,32 @@
+package org.kylin.bst;
+
+public class BSTTree {
+
+    static class BSTNode {
+        int key;
+        Object value;
+        BSTNode left;
+        BSTNode right;
+
+        public BSTNode(int key) {
+            this.key = key;
+        }
+
+        public BSTNode(int key, Object value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public BSTNode(int key, Object value, BSTNode left, BSTNode right) {
+            this.key = key;
+            this.value = value;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    // root node
+    BSTNode root;
+
+
+}
