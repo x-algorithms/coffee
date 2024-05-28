daa2aa3ef0f078be94de2730b62e88ff8c8e2b35
Kylin <unikylin@126.com>
2024-05-28 14:00:37 +0800

    get bst node test case

--- src/main/java/org/kylin/bst/BSTTree.java
+++ src/main/java/org/kylin/bst/BSTTree.java
@@ -1,5 +1,8 @@
 package org.kylin.bst;
 
+/**
+ * BST Binary Search Tree
+ */
 public class BSTTree {
 
     static class BSTNode {
@@ -27,6 +30,29 @@ public class BSTTree {
 
     // root node
     BSTNode root;
+
+    /**
+     * Find BSTNode by key
+     * @param key
+     * @return BST Node
+     */
+    public Object get(int key) {
+        return getHelper(root, key);
+    }
+
+    public Object getHelper(BSTNode node, int key) {
+        if (node == null) {
+            return null;
+        }
+
+        if (key < node.key) {
+            return getHelper(node.left, key);
+        } else if (key > node.key) {
+            return getHelper(node.right, key);
+        } else {
+            return node.value;
+        }
+    }
 
 
 }
